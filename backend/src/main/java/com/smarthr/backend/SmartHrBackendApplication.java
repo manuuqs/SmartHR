bff6ff0326fe84e221915b8599363147c255386d
ManuelQuijada <manuelquijada2000@gmail.com>
2025-12-29 14:51:07 +0100

    Arreglo servicios e imagenes docker para pgadmin

--- backend/src/main/java/com/smarthr/backend/SmartHrBackendApplication.java
+++ backend/src/main/java/com/smarthr/backend/SmartHrBackendApplication.java
@@ -2,8 +2,10 @@ package com.smarthr.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.smarthr.backend.domain")
 public class SmartHrBackendApplication {
 
 	public static void main(String[] args) {
